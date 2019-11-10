9151b381e5e32bced62f418acc7da8c2559deb95
danielevans-999 <danielevans.karani@gmail.comgit>
2019-11-10 18:05:14 +0300

    fixed identations

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    
+class Location(models.Model):
+    loc_name = models.CharField(max_length=30)
+    
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    image_name = models.CharField(max_length=30)
+    image_descprition = models.CharField(max_length=30)
+    location = models.ForeignKey(Location,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
