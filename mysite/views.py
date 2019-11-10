e67c86bd904bdde8342515008d65855315be0752
danielevans-999 <danielevans.karani@gmail.comgit>
2019-11-10 20:36:28 +0300

    Adds a a view function that will render the search term file

--- mysite/views.py
+++ mysite/views.py
@@ -4,3 +4,16 @@ from .models import Image
 def home(request):
     images = Image.objects.all()
     return render(request,'mysite/home.html' ,{"images":images})
+
+def search_results(request):
+    if 'images' in request.GET and request.GET["images"]:
+
+        search_term = request.GET.get("images")
+        searched_Images = Image.search_image(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'mysite/search.html', {"message": message, "Images": searched_Images})
+
+    else:
+        message = "You haven't searched for any term "
+        return render(request, 'mysite/search.html', {"message": message})
