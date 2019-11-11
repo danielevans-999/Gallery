bb82ff5d4dd80a2c43ec455bcab7a680c3338c85
danielevans-999 <danielevans.karani@gmail.comgit>
2019-11-11 12:31:22 +0300

    Adds a test to test the image location instance

--- mysite/tests.py
+++ mysite/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import *
+class EditorTestClass(TestCase):
+    
+    def setUp(self):
+        self.james = Location(loc_name = 'nakuru')
+        
+        
+  #Testing instance
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.james,Location)) 
 
-# Create your tests here.
+
+
+
+    
